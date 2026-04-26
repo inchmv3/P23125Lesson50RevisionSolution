003a7ffedb957994f0364c499aabe15294c2b974
Dmitry <dmitrysamisenko@gmail.com>
2026-04-26 18:07:55 +0300

    task01

--- Task01/logic.cpp
+++ Task01/logic.cpp
@@ -7,3 +7,17 @@
 // Дополнительно в функции необходимо предусмотреть механизм "защиты от дурака"
 // для работы с некорректными данными (в данном случае функция должна 
 // возвращать значение -1).
+#include "logic.h"
+int count_nonzero_elements(int array[], int size) {
+	int count = 0;
+	if (size <= 0) {
+		return -1;
+	}
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] > 0) {
+			count++;
+		}
+	}
+	return count;
+}
