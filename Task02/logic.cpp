2deda1fdbc99226ca8f1f08f495ea3b6ad9320dc
Dmitry <dmitrysamisenko@gmail.com>
2026-04-26 19:56:26 +0300

    add task2 ( не закончил)

--- Task02/logic.cpp
+++ Task02/logic.cpp
@@ -11,7 +11,9 @@
 // 
 // Далее необходимо разработать полноценный или тестовый проект для полной 
 // демонстрации работоспособности данной функции.
+
 #include "logic.h"
+
  double get_average(int array[], int size) {
 	 double average = 0.0;
 	 double s = 0;
